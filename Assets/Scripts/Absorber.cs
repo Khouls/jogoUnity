6eb2b530739d82158ead8034e76f1aa72e081e7c
Khouls <gui.ismael.flach@gmaill.com>
2019-11-27 18:27:37 -0600

    Funciona e graficos bonitos

---
+++ Assets/Scripts/Absorber.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Absorber : MonoBehaviour
+{
+    public PlayerMovement playermov;
+    string objeto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((objeto  == "PedraPulo") && Input.GetButtonDown("Absorb")) {
+            Absorb();
+        }
+        
+    }
+
+    void Absorb()
+    {
+
+        Debug.Log("Aprendeu Pulo");
+        playermov.LearnJump();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        objeto = collision.name;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        objeto = "";
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public SpriteRenderer renderer;
+    public ParticleSystem partSys;
+
+    public float runSpeed = 40f;
+
+
+
+    bool jump = false;
+    bool crouch = false;
+    bool learnedJump = false;
+
+    float horizontalMove = 0f;
+
+
+    // Update is called once per frame
+    void Update() {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        
+        if (learnedJump && Input.GetButtonDown("Jump")) {
+            partSys.startColor = new Color(1, 175f / 255f, 19f / 255f, 1);
+            renderer.color = new Color(1, 175f / 255f, 19f / 255f, 1);
+
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch")){
+            crouch = false;
+        }
+
+
+    }
+
+    public void  OnLanding() {
+        animator.SetBool("IsJumping", false);
+        partSys.startColor = new Color(1, 1, 1, 1);
+        renderer.color = new Color(1, 1, 1, 1);
+    }
+
+    public void OnCrouching (bool isCrouching) {
+        animator.SetBool("IsCrouching", isCrouching);
+    }
+
+    void FixedUpdate() {
+
+        //Move Character
+
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch , jump);
+        jump = false;
+
+    }
+
+    public void LearnJump()
+    {
+        learnedJump = true;
+    }
+}
